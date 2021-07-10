4e2c5cf12f7451deb52014b88cff09cb159fe77e
Parro97 <83281983+Parro97@users.noreply.github.com>
2021-07-10 22:58:57 +0200

    Add files via upload

---
+++ Final_Project.R
@@ -0,0 +1,55 @@
+library(data.table)
+fileurl <- 'https://d396qusza40orc.cloudfront.net/getdata%2Fprojectfiles%2FUCI%20HAR%20Dataset.zip'
+
+setwd("C:/Users/crist/OneDrive/Escritorio/Curso R/Coursera/Cleaning_Data")
+#download.file(fileurl,'./UCI HAR Dataset.zip', mode = 'wb')#, destfile="./Week3") #,destfile = "./Week3/Codebook.csv")
+download.file(fileurl,destfile = './Final_Project/aa.zip')
+unzip("Final_Project/aa.zip",exdir = "./Final_Project")
+
+
+features <- read.csv('./UCI HAR Dataset/features.txt', header = FALSE, sep = ' ')
+features <- as.character(features[,2])
+data.train.x <- read.table('./UCI HAR Dataset/train/X_train.txt')
+data.train.y <- read.csv('./UCI HAR Dataset/train/y_train.txt',header = FALSE, sep = ' ')
+data.train.subject <- read.csv('./UCI HAR Dataset/train/subject_train.txt',header = FALSE, sep = ' ')
+
+data.train <-  data.frame(data.train.subject, data.train.y, data.train.x)
+names(data.train) <- c(c('subject', 'activity'), features)
+
+data.test.x <- read.table('./UCI HAR Dataset/test/X_test.txt')
+data.test.y <- read.csv('./UCI HAR Dataset/test/y_test.txt', header = FALSE, sep = ' ')
+data.test.subject <- read.csv('./UCI HAR Dataset/test/subject_test.txt', header = FALSE, sep = ' ')
+
+data.test <-  data.frame(data.test.subject, data.test.y, data.test.x)
+names(data.test) <- c(c('subject', 'activity'), features)
+
+####Merges the Training and Testing Sets#####
+
+data <- rbind(data.train, data.test)
+
+####. Extracts only the measurements on the mean 
+#### and standard deviation for each measurement
+mean_std.select <- grep('mean|std', features)
+data.sub <- data[,c(1,2,mean_std.select + 2)]
+#### Uses descriptive activity names to name 
+####the activities in the data set
+activity.labels <- read.table('./UCI HAR Dataset/activity_labels.txt', header = FALSE)
+activity.labels <- as.character(activity.labels[,2])
+data.sub$activity <- activity.labels[data.sub$activity]
+####Appropriately labels the data set with 
+####descriptive variable names.
+name.new <- names(data.sub)
+name.new <- gsub("[(][)]", "", name.new)
+name.new <- gsub("^t", "TimeDomain_", name.new)
+name.new <- gsub("^f", "FrequencyDomain_", name.new)
+name.new <- gsub("Acc", "Accelerometer", name.new)
+name.new <- gsub("Gyro", "Gyroscope", name.new)
+name.new <- gsub("Mag", "Magnitude", name.new)
+name.new <- gsub("-mean-", "_Mean_", name.new)
+name.new <- gsub("-std-", "_StandardDeviation_", name.new)
+name.new <- gsub("-", "_", name.new)
+names(data.sub) <- name.new
+#### tidy data set with the average of each variable 
+#### for each activity and each subject
+data.tidy <- aggregate(data.sub[,3:81], by = list(activity = data.sub$activity, subject = data.sub$subject),FUN = mean)
+write.table(x = data.tidy, file = "Final_Project/data_tidy.txt", row.names = FALSE)
